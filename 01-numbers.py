1a3225ff409bc8aef51bc6421ea5f23247589878
Sagar Acharya <sagar_acharya@live.in>
2017-03-28 00:06:13 +0530

    first commit

---
+++ 01-numbers.py
@@ -0,0 +1,8 @@
+#from __future__ import division #used to import python3 features
+print 3/2   #Answer in python2 : 1 in python3 : 1.5
+print 2 + 3
+print 3 - 2
+salary_amount = 100
+interest_rate = 0.1
+final_amount = salary_amount * interest_rate
+print final_amount
